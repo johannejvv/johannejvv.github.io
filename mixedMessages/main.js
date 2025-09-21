24074d9c0d272874320e69c4a1458ea1ce6e8b0f
johannejvv <johannejvv@gmail.com>
2025-09-21 15:15:54 +0100

    add message components

--- mixedMessages/main.js
+++ mixedMessages/main.js
@@ -2,6 +2,11 @@
 
 //output layout
 console.log('');
-console.log("=========================================");
-console.log("For dinner you are having shephard's pie!");
-console.log("================== <3 ===================");
+console.log("=======================================================");
+console.log("For dinner you are having shephard's pie and ice cream!");
+console.log("================== <3 =================================");
+
+
+const mealTime = ["breakfast", "lunch", "dinner"];
+const mains = ["shepard's pie", "pizza", "scrambled eggs", "waffles", "butternut soup"];
+const dessert = ["ice cream", "donuts", "fruit salad", "chocolate mousse", "creme brulee"];
