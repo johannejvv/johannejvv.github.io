0bb6ebffca3e49346f1f3108706ba2e4221ff3f6
johannejvv <johannejvv@gmail.com>
2025-09-21 15:31:58 +0100

    create random message and log to console

--- mixedMessages/main.js
+++ mixedMessages/main.js
@@ -1,12 +1,28 @@
 
 
 //output layout
-console.log('');
+/*console.log('');
 console.log("=======================================================");
 console.log("For dinner you are having shephard's pie and ice cream!");
-console.log("================== <3 =================================");
+console.log("================== <3 =================================");*/
 
 
-const mealTime = ["breakfast", "lunch", "dinner"];
-const mains = ["shepard's pie", "pizza", "scrambled eggs", "waffles", "butternut soup"];
-const dessert = ["ice cream", "donuts", "fruit salad", "chocolate mousse", "creme brulee"];
+const mealTimeArr = ["breakfast", "lunch", "dinner"];
+const mainsArr = ["shepard's pie", "pizza", "scrambled eggs", "waffles", "butternut soup"];
+const dessertArr = ["ice cream", "donuts", "fruit salad", "chocolate mousse", "creme brulee"];
+
+let selectMealTime = Math.floor(Math.random()*(mealTimeArr.length));
+let selectMains = Math.floor(Math.random()*(mainsArr.length));
+let selectDessert = Math.floor(Math.random()*(dessertArr.length));
+
+function createMessage () {
+  let mealTime = mealTimeArr[selectMealTime];
+  let mains = mainsArr[selectMains];
+  let dessert = dessertArr[selectDessert];
+
+  console.log("==============================================================");
+  console.log(`For ${mealTime} you are having ${mains} and ${dessert}!`);
+  console.log("================== <3 ========================================");
+};
+
+createMessage();
