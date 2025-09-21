9db247149b5613008d1eb72956cc121c2d8bd8d1
johannejvv <johannejvv@gmail.com>
2025-09-21 15:07:36 +0100

    add first message printout

---
+++ mixedMessages/main.js
@@ -0,0 +1,7 @@
+
+
+//output layout
+console.log('');
+console.log("=========================================");
+console.log("For dinner you are having shephard's pie!");
+console.log("================== <3 ===================");
